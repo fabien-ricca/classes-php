74e4ad0de9083401284074e36c30ae24218fc79a
fabien-ricca <fabien.ricca@laplateforme.io>
2023-01-02 15:06:07 +0100

    fonction register

--- User.php
+++ User.php
@@ -1,17 +1,35 @@
 <?php 
+
+    $connect = mysqli_connect('localhost', 'root', '', 'classes');
+
+    
     class User {
         private $id;
-        private $login;
-        private $email;
-        private $firstname;
-        private $lastname;
+        public $login;
+        public $email;
+        public $firstname;
+        public $lastname;
 
-        public function __construct($login, $email, $firstname, $lastname){
-            $this->login = $login;
-            $this->email = $email;
-            $this->firstname = $firstname;
-            $this->lastname = $lastname;
+        public function __construct(){
+        }
+
+        public function register($login, $password, $email, $firstname, $lastname){
+            global $connect;
+            $request = $connect->query("INSERT INTO `utilisateurs`(`login`, `password`, `email`, `firstname`, `lastname`)
+                                             VALUES ('$login', '$password', '$email', '$firstname', '$lastname')");
+            echo "L'utilisateur {$login} a bien été créé.";
         }
     }
 
+    $padawan = new User();
+    $padawan->register('Chtulhu', 'bonjour', 'anna@gmail.com', 'Anna', 'Gilg');
+
+    //$padawan = new User('padawan', 'padawan@gmail.com', 'Fabien', 'Ricca');
+
+    //var_dump($padawan);
+
+    /*$req = "SELECT * FROM `utilisateurs`";
+    $request = mysqli_query($connect, $req);
+    $data = mysqli_fetch_assoc($request);
+    var_dump($data);*/
 ?>
